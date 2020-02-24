be6640c6cb5d92ac7a8bd25d7719042d04eb6136
Hillel Coren <hillelcoren@gmail.com>
2020-02-24 21:11:23 +0200

    Update

---
+++ invoiceninja/flutter_service_worker.js
@@ -0,0 +1,42 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/assets\AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
+"/assets\assets\images\logo.png": "090f69e23311a4b6d851b3880ae52541",
+"/assets\FontManifest.json": "3fca5e5514468a50735f8318fbb836e4",
+"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets\fonts\Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"/assets\LICENSE": "c1e79190402549e69f0bc14cf3026ae3",
+"/assets\packages\font_awesome_flutter\lib\fonts\fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
+"/assets\packages\font_awesome_flutter\lib\fonts\fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
+"/assets\packages\font_awesome_flutter\lib\fonts\fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
+"/assets\web\assets\fonts\Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"/index.html": "8eb9ba9d5065fe2977d9efbbeaea3f9f",
+"/main.dart.js": "0c276ed2ac3d39a2b051e6bc036f9ebe"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request, {
+          credentials: 'include'
+        });
+      })
+  );
+});
