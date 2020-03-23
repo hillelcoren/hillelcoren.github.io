567151ffc1c29d8fb457a581d48ef16d5d1d9616
Hillel Coren <hillelcoren@gmail.com>
2020-03-23 19:50:26 +0200

    Update demo

--- invoiceninja/flutter_service_worker.js
+++ invoiceninja/flutter_service_worker.js
@@ -1,18 +1,19 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
-"/assets\assets\images\logo.png": "090f69e23311a4b6d851b3880ae52541",
-"/assets\FontManifest.json": "3fca5e5514468a50735f8318fbb836e4",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\fonts\Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"/assets\LICENSE": "c1e79190402549e69f0bc14cf3026ae3",
-"/assets\packages\font_awesome_flutter\lib\fonts\fa-brands-400.ttf": "3ca122272cfac33efb09d0717efde2fa",
-"/assets\packages\font_awesome_flutter\lib\fonts\fa-regular-400.ttf": "bdd8d75eb9e6832ccd3117e06c51f0d3",
-"/assets\packages\font_awesome_flutter\lib\fonts\fa-solid-900.ttf": "d21f791b837673851dd14f7c132ef32e",
-"/assets\web\assets\fonts\Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"/index.html": "8eb9ba9d5065fe2977d9efbbeaea3f9f",
-"/main.dart.js": "0e9aaf771b959f6b5e00461ba6caebbe"
+  "/assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
+"/assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
+"/assets/FontManifest.json": "3fca5e5514468a50735f8318fbb836e4",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"/assets/LICENSE": "9b23f493027d96b1d739228ff7c02d38",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "51d23d1c30deda6f34673e0d5600fd38",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "d51b09f7b8345b41dd3b2201f653c62b",
+"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "0ea892e09437fcaa050b2b15c53173b7",
+"/assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"/favicon.ico": "51636d3a390451561744c42188ccd628",
+"/index.html": "7bf06ad8ee733460dd4e4249f91b3745",
+"/main.dart.js": "ce0cc00e15c5bbefcce45d87127c2d51"
 };
 
 self.addEventListener('activate', function (event) {
@@ -34,9 +35,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
