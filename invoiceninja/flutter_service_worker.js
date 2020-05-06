7e54016df71f819b9d3d68809850e5f82d8c2a83
Hillel Coren <hillelcoren@gmail.com>
2020-05-06 23:40:16 +0300

    Update web demo

--- invoiceninja/flutter_service_worker.js
+++ invoiceninja/flutter_service_worker.js
@@ -1,19 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
-"/assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
-"/assets/FontManifest.json": "3fca5e5514468a50735f8318fbb836e4",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"/assets/LICENSE": "9b23f493027d96b1d739228ff7c02d38",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "51d23d1c30deda6f34673e0d5600fd38",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "d51b09f7b8345b41dd3b2201f653c62b",
-"/assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "0ea892e09437fcaa050b2b15c53173b7",
-"/assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
-"/favicon.ico": "51636d3a390451561744c42188ccd628",
-"/index.html": "7bf06ad8ee733460dd4e4249f91b3745",
-"/main.dart.js": "88b3f5eee3bbba3d59eb3e6fe0e17de0"
+  "assets/AssetManifest.json": "90d7b27343fcf361dbf6385b3055ce4f",
+"assets/assets/images/logo.png": "090f69e23311a4b6d851b3880ae52541",
+"assets/FontManifest.json": "3fca5e5514468a50735f8318fbb836e4",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"assets/LICENSE": "7ef0c06ed19004a447a04832bad41b03",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/web/assets/fonts/Roboto-Regular.ttf": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+"favicon.ico": "51636d3a390451561744c42188ccd628",
+"index.html": "7bf06ad8ee733460dd4e4249f91b3745",
+"/": "7bf06ad8ee733460dd4e4249f91b3745",
+"main.dart.js": "0dace259e3c843bb3d8821deb98bc391"
 };
 
 self.addEventListener('activate', function (event) {
