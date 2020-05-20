32325d5b400f64009560e501211a1db3b601a275
Hillel Coren <hillelcoren@gmail.com>
2020-05-20 11:15:51 +0300

    Update build

--- invoiceninja/flutter_service_worker.js
+++ invoiceninja/flutter_service_worker.js
@@ -16,7 +16,7 @@ const RESOURCES = {
 "favicon.ico": "51636d3a390451561744c42188ccd628",
 "index.html": "7bf06ad8ee733460dd4e4249f91b3745",
 "/": "7bf06ad8ee733460dd4e4249f91b3745",
-"main.dart.js": "9d2690d4eb0557eb84e048a35b0f5f19"
+"main.dart.js": "6e4700f2978ccfa63a2292ae6cdac814"
 };
 
 // The application shell files that are downloaded before a service worker can
